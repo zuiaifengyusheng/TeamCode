9d6d47cdda6c72e1b7fd679a62517af298077c96
wang <634957209@qq.com>
2016-09-11 21:58:37 +0800

    找不到优化方法

--- FileTransfer/src/com/example/Service/FileInforService.java
+++ FileTransfer/src/com/example/Service/FileInforService.java
@@ -1,27 +1,14 @@
 package com.example.Service;
 
 import java.io.File;
-import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 
 
 import android.annotation.SuppressLint;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.database.Cursor;
-import android.media.ExifInterface;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.provider.MediaStore.Files;
-import android.provider.MediaStore.Images;
-import android.widget.Toast;
 
-import com.bumptech.glide.load.model.file_descriptor.FileDescriptorFileLoader;
 import com.example.shuxing.FileInfor;
-import com.example.shuxing.VideoInfor;
 
 
 @SuppressLint("NewApi")
@@ -45,22 +32,7 @@ public class FileInforService {
 		afile=new ArrayList<File>();
 		File file=Environment.getExternalStorageDirectory();
 		File[] files=file.listFiles();
-		runFile(files);
-
-		/*Thread thread=new Thread(){
-
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				for(int i=0;i<afile.size();i++)
-				{
-					
-				}
-				
-			}  	            		 
-       	 };
- 		 thread.start(); 
-		Toast.makeText(context, listfile.size()+"", Toast.LENGTH_SHORT).show();*/
+		runFile(files);	
 	    return afile;
 	}
 	public void runFile(File[] file)
@@ -74,26 +46,6 @@ public class FileInforService {
   	            	  System.out.println(file2.isFile());
 	            	  if(file2.isFile())
 	            	  {
-	            		 /* String str = "文件名称:" + file2.getName() + " 路径:"+ file2.getPath();  
-		                  System.out.println(str);  
-		            	  FileInfor fileInfor=new FileInfor();
-		            	  fileInfor.setName(file2.getName());
-		            	  fileInfor.setPath(file2.getAbsolutePath());
-		            	  int size=(int)file2.length();
-		            	  BigDecimal big=parseApkSize(size);	            	  
-		            	  fileInfor.setSize(big.toString());
-		            	  String date=null;
-		            	  ExifInterface exif;
-						  try {
-							exif = new ExifInterface(file2.getPath());
-							date=exif.getAttribute(ExifInterface.TAG_DATETIME);
-						  } catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						  }
-		 			      
-		            	  fileInfor.setData(date);
-		            	  lpath.add(fileInfor); */
 	            		  afile.add(file2);
 		              }
 	              }
@@ -114,75 +66,5 @@ public class FileInforService {
   	          }
   	     }
 	}
-/*	public class MyFile extends AsyncTask<String, Integer, ArrayList<FileInfor>> {
 
-		
-        @SuppressLint("NewApi")
-		@Override
-        protected ArrayList<FileInfor> doInBackground(String... params) {
-        	ArrayList<FileInfor> lpath=new ArrayList<FileInfor>();
-        	File file=Environment.getExternalStorageDirectory();
-            Uri uri = MediaStore.Files.getContentUri(file.getAbsolutePath());
-            
-            Toast.makeText(context,uri+"", Toast.LENGTH_SHORT).show();
-            ContentResolver mContentResolver =context.getContentResolver();
-			Cursor cursor=mContentResolver.query(uri, null, null, null,null);
-			Toast.makeText(context, cursor.getCount()+"", Toast.LENGTH_SHORT).show();
-            if(uri.getScheme().equals("file"))
-            {
-            	String path=uri.getEncodedPath();
-            	if(path!=null)
-            	{
-            		path=Uri.decode(path);
-            		ContentResolver cr=context.getContentResolver();
-            		buff.append("(").append(Files.FileColumns.DATA).append("=").append(""+path+"").append(")");
-            		
-            		 Toast.makeText(context, buff, Toast.LENGTH_SHORT).show();
-            	}
-            
-            
-            
-            Toast.makeText(context, , Toast.LENGTH_SHORT).show();
-            
-            ContentResolver mContentResolver =context.getContentResolver();
-			Cursor cursor=mContentResolver.query(mVideoUri, null, null, null,null);
-			Toast.makeText(context, cursor.getCount()+"", Toast.LENGTH_SHORT).show();
-			while(cursor.moveToNext())
-			{
-				FileInfor video=new FileInfor();
-				
-				int id=cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID));
-				String name=cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME));
-				String path=cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA));
-				String data=cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_MODIFIED));
-				long ltime=cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION));
-				int lsize=cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE));
-				BigDecimal pSize=parseApkSize(lsize);
-		        String size=pSize.toString();
-				video=new FileInfor(name,path,data,size);
-				lpath.add(video);
-				
-			}
-			cursor.close();
-			return lpath;
-        }
-        public void filePath(File file) {  
-        	
-    		Toast.makeText(context, file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-        	
-    		
-    		
-        }
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<FileInfor> bytes) {
-            super.onPostExecute(bytes);
-        }
-
-	}*/
-	
 }
--- FileTransfer/src/com/example/filetransfer/FileFragment.java
+++ FileTransfer/src/com/example/filetransfer/FileFragment.java
@@ -3,15 +3,20 @@ package com.example.filetransfer;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.example.Service.FileInforService;
+import com.example.adapter.FileAdapter;
 
+import com.example.listener.OneListItemExpandListener;
 import com.example.shuxing.FileInfor;
 
+import android.content.Context;
 import android.media.ExifInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -19,33 +24,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
 
 public class FileFragment extends Fragment {
 
 	
-	private FileInforService service;
+	
 	static List<Map<String,Object>> listFile;
-	private List<Map<String,Object>> listWord;
-	private List<Map<String,Object>> listPpt;
-	private List<Map<String,Object>> listExcel;
-	private List<Map<String,Object>> listZip;
-	private List<Map<String,Object>> listPdf;
+	
+	public ExpandableListView list_file;
+	public FileAdapter fileAdapter;
 	@Override
 	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onViewCreated(view, savedInstanceState);
-		service=new FileInforService(getActivity());
-		/*list=service.getAllFileList();*/
-		/*Toast.makeText(getActivity(), list.get(0)+"", Toast.LENGTH_SHORT).show();*/
-		/*listFile=getFileList();*/
-		listExcel=new ArrayList<Map<String,Object>>();
-		listPdf=new ArrayList<Map<String,Object>>();
-		listWord=new ArrayList<Map<String,Object>>();
-		listPpt =new ArrayList<Map<String,Object>>();
-		listZip=new ArrayList<Map<String,Object>>();
-		getFileList();
 		
+		String[] groupList=new String[5];
+		groupList[0]="Word文件";
+		groupList[1]="Ppt文件";
+		groupList[2]="Excel文件";
+		groupList[3]="电子书";
+		groupList[4]="压缩文件";
+		
+		
+		//找到这个控件  
+        list_file= (ExpandableListView)view.findViewById(R.id.listFile);  
+          
+        list_file.setAdapter(new FileAdapter(getActivity(), list_file, groupList));
+        list_file.expandGroup(0);
+        list_file.setOnGroupExpandListener(new OneListItemExpandListener(list_file)); 		
 		
 	}
 
@@ -56,118 +64,5 @@ public class FileFragment extends Fragment {
 		return inflater.inflate(R.layout.activity_file,container, false);
 	}
 	
-	private ArrayList<FileInfor> getFileList()
-	{
-
-		ArrayList<File> files=new ArrayList<File>();
-		service=new FileInforService(getActivity());
-		
-		files=service.getAllFileList();
-		
-		int fsize=files.size();
-		if(fsize==0)
-		{
-			return null;
-		}
-		for(int i=0;i<fsize;i++)
-		{
-			File file=files.get(i);
-			String name=file.getName();
-			int a=0;
-			if(name.endsWith("xls")||name.endsWith("xlsx")||name.endsWith("xlsm")||name.endsWith("xltm")||name.endsWith("xlsb")||name.endsWith("xlam"))
-			{
-/*				Bitmap icon=BitmapFactory.decodeResource(getResources(), R.drawable.excel);	*/
-				a=1;
-			}
-			if(name.endsWith("pptx")||name.endsWith("pptm")||name.endsWith("ppsx")||name.endsWith("potx")||name.endsWith("potm")||name.endsWith("ppam"))
-			{
-				a=2;
-			}
-			if(name.endsWith("docx")||name.endsWith("docm")||name.endsWith("dotx")||name.endsWith("dotm"))
-			{
-				a=3;
-			}
-			if(name.endsWith("umd")||name.endsWith("pdf")||name.endsWith("txt")||name.endsWith("edk")||name.endsWith("chm"))
-			{
-				a=4;
-			}
-			if(name.endsWith("zip")||name.endsWith("rar")||name.endsWith("iso")||name.endsWith("7z"))
-			{
-				a=5;
-			}
-			if(a==0)
-			{
-				break;
-			}
-		    Map<String,Object> map=new HashMap<String,Object>();
-		    String str = "文件名称:" + file.getName() + " 路径:"+ file.getPath();  
-            System.out.println(str);  
-            FileInfor fileInfor=new FileInfor();           
-            String path=file.getAbsolutePath();
-            int size=(int)file.length();         	            	  
-            String fileSize=parseApkSize(size);
-            String date=null;
-            ExifInterface exif;
-			try {
-				exif = new ExifInterface(file.getPath());
-				date=exif.getAttribute(ExifInterface.TAG_DATETIME);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			map.put("name", name);
-			Toast.makeText(getActivity(), fileInfor.getName(),Toast.LENGTH_SHORT).show();
-			map.put("path", path);
-			map.put("date",date);
-		    map.put("size", fileSize);
-		    
-		    if(a==1)
-		    {
-		    	map.put("icon", R.drawable.excel);
-		    	listExcel.add(map);
-		    }
-			if(a==2)
-			{
-				map.put("icon", R.drawable.ppt);
-				listPpt.add(map);
-			}
-			if(a==3)
-		    {
-				map.put("icon", R.drawable.word);
-		    	listWord.add(map);
-		    }
-			if(a==4)
-			{
-				map.put("icon", R.drawable.pdf);
-				listPdf.add(map);
-			}
-			if(a==5)
-		    {
-				map.put("icon", R.drawable.zip);
-		    	listZip.add(map);
-		    }
-			
-		}
-		return null;
-	}
-	private String parseApkSize(int size) {
-		BigDecimal bd;
-		BigDecimal setScale;
-		String str=null;
-		if(size/1024>0)
-		{
-			bd=new BigDecimal((double)size/(1024));
-			setScale = bd.setScale(2, BigDecimal.ROUND_DOWN);
-			str=setScale.toString()+"KB";  
-		}
-		else
-		{
-			bd = new BigDecimal((double)size/(1024*1024));
-			setScale = bd.setScale(2, BigDecimal.ROUND_DOWN);
-			str=setScale.toString()+"M";  
-		}
-	    
-	          
-	    return str;
-	}
+	
 }
